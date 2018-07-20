aa6bbd23084949281e99eb4e3f35990b97b47400
tim-hub <tim_w@outlook.com>
2018-07-21 10:59:02 +1200

    update button.cs, add debug for collider enter

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Button : Interactable
 {
-	public Interactable interactable;
+    
+    public Interactable interactable;
 
-  void OnTriggerEnter(Collider other)
-  {
-		interactable.Interact();
-  }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+    	interactable.Interact();
+    }
 
-  void OnTriggerExit(Collider other)
-  {
-		interactable.Interact();
-  }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name);
+	    interactable.Interact();
+    }
 }
