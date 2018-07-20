dbb520f13cfa28cac877aaf88302cf212a90f5c4
uki <jasperburtmason@gmail.com>
2018-07-21 09:58:11 +1200

    Fix step counter

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,39 +4,41 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-  public int maximumSteps;
-  public float movementSpeed;
+    public int maximumSteps;
+    public float movementSpeed;
 
-  bool moving;
-  int steps;
-  float delta;
+    bool moving;
+    int steps;
+    float delta;
 
-  public void Move(Vector2Int direction)
-  {
-    if (!moving && steps < maximumSteps)
+    public void Move(Vector2Int direction)
     {
-      if (!Physics2D.Raycast(transform.position, direction, 0.5f))
-        StartCoroutine(IMove(direction));
-
-      steps++;
+      if (!moving && steps < maximumSteps)
+      {
+        // Check if there is room to move
+        if (!Physics2D.Raycast(transform.position, direction, 0.5f))
+        {
+          StartCoroutine(IMove(direction));
+          steps++;
+        }
+      }
     }
-  }
 
-  IEnumerator IMove(Vector2Int direction)
-  {
-    float time = delta;
-    Vector2 startPosition = transform.position;
-    Vector2 endPosition = startPosition + direction;
-
-    moving = true;
-    while (time < 1f)
+    IEnumerator IMove(Vector2Int direction)
     {
-      transform.position = Vector2.Lerp(startPosition, endPosition, time);
-      time += Time.deltaTime * movementSpeed;
-      yield return null;
+      float time = delta;
+      Vector2 startPosition = transform.position;
+      Vector2 endPosition = startPosition + direction;
+
+      moving = true;
+      while (time < 1f)
+      {
+        transform.position = Vector2.Lerp(startPosition, endPosition, time);
+        time += Time.deltaTime * movementSpeed;
+        yield return null;
+      }
+      transform.position = endPosition;
+      delta = time - 1f;
+      moving = false;
     }
-    transform.position = endPosition;
-    delta = time - 1f;
-    moving = false;
-  }
 }
