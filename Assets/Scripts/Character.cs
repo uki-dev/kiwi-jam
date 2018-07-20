843278acf830bd770e89de1ee6551404820e2058
uki <contact@uki.nz>
2018-07-21 01:02:26 +1200

    Basic movement

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour
+{
+  public float speed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  bool moving;
+  float delta;
+
+  public void Move(Vector2Int direction)
+  {
+    if (!moving)
+      StartCoroutine(IMove(direction));
+  }
+
+  private IEnumerator IMove(Vector2Int direction)
+  {
+    float time = delta;
+    Vector2 startPosition = transform.position;
+    Vector2 endPosition = startPosition + direction;
+
+    moving = true;
+    while (time < 1f)
+    {
+      transform.position = Vector2.Lerp(startPosition, endPosition, time);
+      time += Time.deltaTime * speed;
+      yield return null;
+    }
+    transform.position = endPosition;
+    delta = time - 1f;
+    moving = false;
+  }
 }
