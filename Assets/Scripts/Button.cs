e1d43f57872bba0db66446b2bc33bb8b6526bdcc
uki <contact@uki.nz>
2018-07-21 10:28:15 +1200

    Add interactable classes

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : Interactable
+{
+	public Interactable interactable;
+
+  void OnTriggerEnter(Collider other)
+  {
+		interactable.Interact();
+  }
+
+  void OnTriggerExit(Collider other)
+  {
+		interactable.Interact();
+  }
+}
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,41 +4,45 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public int maximumSteps;
-    public float movementSpeed;
+  public int maximumSteps;
+  public float movementSpeed;
 
-    bool moving;
-    int steps;
-    float delta;
+  [HideInInspector]
+  public Vector2Int direction;
 
-    public void Move(Vector2Int direction)
+  bool moving;
+  int steps;
+  float delta;
+
+  public void Move(Vector2Int direction)
+  {
+    if (!moving && steps < maximumSteps)
     {
-      if (!moving && steps < maximumSteps)
+      // Check if there is room to move
+      if (!Physics2D.Raycast(transform.position, direction, 0.5f))
       {
-        // Check if there is room to move
-        if (!Physics2D.Raycast(transform.position, direction, 0.5f))
-        {
-          StartCoroutine(IMove(direction));
-          steps++;
-        }
+        this.direction = direction;
+        StartCoroutine(IMove(direction));
+        steps++;
       }
     }
+  }
 
-    IEnumerator IMove(Vector2Int direction)
+  IEnumerator IMove(Vector2Int direction)
+  {
+    float time = delta;
+    Vector2 startPosition = transform.position;
+    Vector2 endPosition = startPosition + direction;
+
+    moving = true;
+    while (time < 1f)
     {
-      float time = delta;
-      Vector2 startPosition = transform.position;
-      Vector2 endPosition = startPosition + direction;
-
-      moving = true;
-      while (time < 1f)
-      {
-        transform.position = Vector2.Lerp(startPosition, endPosition, time);
-        time += Time.deltaTime * movementSpeed;
-        yield return null;
-      }
-      transform.position = endPosition;
-      delta = time - 1f;
-      moving = false;
+      transform.position = Vector2.Lerp(startPosition, endPosition, time);
+      time += Time.deltaTime * movementSpeed;
+      yield return null;
     }
+    transform.position = endPosition;
+    delta = time - 1f;
+    moving = false;
+  }
 }
