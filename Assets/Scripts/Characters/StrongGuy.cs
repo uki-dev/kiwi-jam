46345132cd9caa952414b27d36cff7fa9a135c0d
uki <contact@uki.nz>
2018-07-21 14:01:52 +1200

    Add new character and enemy classes, add door opening and closing,

---
+++ Assets/Scripts/Characters/StrongGuy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrongGuy : Character
+{
+  protected override bool CanMove(Vector2Int direction)
+  {
+    bool canMove = base.CanMove(direction);
+    if (!canMove && Time.time >= nextMove && steps < maximumSteps)
+    {
+      RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, direction, 0.5f, LayerMask.NameToLayer("Box"));
+      if (raycastHit)
+      {
+        Box box = raycastHit.collider.GetComponent<Box>();
+        if (box && box.Move(direction))
+          return true;
+      }
+    }
+    return canMove;
+  }
+}
