3db7ec5aded833055f69e066f58e98a8cfa8b5f4
uki <contact@uki.nz>
2018-07-21 18:27:44 +1200

    Add Long Guy, add pits, fix movement code, update test level

--- Assets/Scripts/Characters/LongGuy.cs
+++ Assets/Scripts/Characters/LongGuy.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LongGuy : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class LongGuy : Character
+{
+  public override bool Move(Vector2Int direction)
+  {
+    if (canMove)
+    {
+      // Check if there is a pit in the direction of movement, otherwise move normally
+      if (Physics2D.Raycast(transform.position, direction, 0.5f, 1 << LayerMask.NameToLayer("Pit")))
+      {
+        // There is a pit, now check if there is an empty space on the other side
+        Vector3 position = transform.position + (Vector3)(Vector2)direction * 2;
+				Debug.Log(Physics2D.OverlapBox(position, Vector2.one * 0.5f, 0f));
+        if (!Physics2D.OverlapBox(position, Vector2.one * 0.5f, 0f))
+        {
+          //There is empty space on the other side so let's move the character
+          this.direction = direction;
+          transform.position = position;
+          nextMove = Time.time + 1f / movementSpeed;
+          return true;
+        }
+      }
+      else
+      {
+        return base.Move(direction);
+      }
+    }
+    return false;
+  }
 }
