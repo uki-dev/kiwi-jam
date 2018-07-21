2106a08f00e71fa3d56340594f52ce67ca830595
MSI\MSNov <MSNoverflow@gmail.com>
2018-07-22 09:39:36 +1200

    Added background music

---
+++ Assets/music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class music : MonoBehaviour {
+    
+
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        audio.Play();
+    }
+    void Update()
+    {
+        if (true)//!audio.isPlaying && audio.clip.isReadyToPlay)
+        {
+           
+        }
+    }
+
+}
