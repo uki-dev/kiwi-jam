ccfbf3131715e6b70463c421cf416026004497f8
uki <contact@uki.nz>
2018-07-21 19:02:08 +1200

    Add exit class, improve box lodging

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -9,39 +9,37 @@ using UnityEngine.Tilemaps;
 
 public class Box : MonoBehaviour
 {
-  bool lodged;
+  [HideInInspector]
+  public Tile lodgedTile;
 
   public bool Move(Vector2Int direction)
   {
-    if (!lodged)
+    // Check first if there's a pit in the movement direction that the box can be lodged into
+    RaycastHit2D pitRaycastHit = Physics2D.Raycast(transform.position, direction, 0.5f, 1 << LayerMask.NameToLayer("Pit"));
+    if (pitRaycastHit)
     {
-      // Check first if there's a pit in the movement direction that the box can be lodged into
-      RaycastHit2D pitRaycastHit = Physics2D.Raycast(transform.position, direction, 0.5f, 1 << LayerMask.NameToLayer("Pit"));
-      if (pitRaycastHit)
-      {
-        // Remove collision on the tilemap and the box
-        Tilemap tilemap = pitRaycastHit.collider.GetComponent<Tilemap>();
-        // Flooring this doesn't work, guess we just - 0.5f?
-        tilemap.SetTile(tilemap.layoutGrid.WorldToCell(pitRaycastHit.point - new Vector2(0.5f, 0.5f)), null);
-        gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
-        transform.position += (Vector3)(Vector2)direction;
-        lodged = true;
-        return true;
-      }
-
-      bool blocked = false;
-      RaycastHit2D[] raycastHits = Physics2D.RaycastAll(transform.position, direction, 0.5f);
-      foreach (RaycastHit2D raycastHit in raycastHits)
-      {
-        if (raycastHit.collider.gameObject != gameObject)
-          blocked = true;
-      }
-      if (!blocked)
-      {
-        transform.position += (Vector3)(Vector2)direction;
-        return true;
-      }
+      // Change the pit tile to the lodged one
+      Tilemap tilemap = pitRaycastHit.collider.GetComponent<Tilemap>();
+      // Flooring this doesn't work, guess we just - 0.5f?
+      tilemap.SetTile(tilemap.layoutGrid.WorldToCell(pitRaycastHit.point - new Vector2(0.5f, 0.5f)), lodgedTile);
+      // Destroy the box
+      GameObject.Destroy(gameObject);
+      return true;
     }
+
+    bool blocked = false;
+    RaycastHit2D[] raycastHits = Physics2D.RaycastAll(transform.position, direction, 0.5f);
+    foreach (RaycastHit2D raycastHit in raycastHits)
+    {
+      if (raycastHit.collider.gameObject != gameObject)
+        blocked = true;
+    }
+    if (!blocked)
+    {
+      transform.position += (Vector3)(Vector2)direction;
+      return true;
+    }
+
     return false;
   }
 }
