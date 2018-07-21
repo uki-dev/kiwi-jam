05e947c8dd25e61d03618a3601879e5c92806eb8
MSI\MSNov <MSNoverflow@gmail.com>
2018-07-21 14:37:59 +1200

    Minor commenting

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿//******************************************************
+//*Date: 21/07/2018
+//******************************************************
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,12 +26,13 @@ public class Character : MonoBehaviour
     // stop diagonal movement
     if(Mathf.Abs(direction.x) == Mathf.Abs(direction.y))
       return false;
-
+    //If its time to move and we have steps left
     if (Time.time >= nextMove && (steps == 0 || steps < maximumSteps))
     {
       RaycastHit2D[] raycastHits = Physics2D.RaycastAll(transform.position, direction, 0.5f);
       foreach (RaycastHit2D raycastHit in raycastHits)
       {
+        //if the raycast hits nothing. (end and return false)
         if (raycastHit.collider.gameObject != gameObject)
           return false;
       }
