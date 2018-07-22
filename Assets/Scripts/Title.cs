46d97493b96e01f9b8913620c6f9df6237bec2c0
uki <contact@uki.nz>
2018-07-22 16:20:56 +1200

    Added title scene and updated main scene

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+  public string level;
+
+  void Update()
+  {
+    if (Input.anyKeyDown)
+    {
+      SceneManager.LoadScene(level);
+    }
+  }
+}
